7586510934f0f04c00c41346d8dfc907ab0aafe3
ouyinheng <2675379992@qq.com>
2019-05-09 13:32:45 +0800

    update

---
+++ list.js
@@ -0,0 +1,15 @@
+const list = [
+  {
+    "name": "我的简历",
+    "alt": "resume",
+    "link": "./project/resume"
+  }, {
+    "name": "时间轮盘",
+    "alt": "soothsaying",
+    "link": "./project/soothsaying"
+  }, {
+    "name": "git",
+    "alt": "git",
+    "link": "./project/git"
+  }
+]
