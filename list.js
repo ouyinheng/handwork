9ec235b6d8c61279aee8aea6d4f9e986caa90753
ouyinheng <2675379992@qq.com>
2019-05-13 10:49:53 +0800

    update

--- list.js
+++ list.js
@@ -2,14 +2,22 @@ const list = [
   {
     "name": "我的简历",
     "alt": "resume",
-    "link": "./src/view/resume"
+    "link": "./src/view/resume/index.html"
   }, {
     "name": "时间轮盘",
     "alt": "soothsaying",
-    "link": "./src/view/soothsaying"
+    "link": "./src/view/soothsaying/index.html"
   }, {
     "name": "git",
     "alt": "git",
-    "link": "./src/view/git"
+    "link": "./src/view/git/index.html"
+  }, {
+    "name": "防抖",
+    "alt": "debounce",
+    "link": "./src/view/debounce/index.html"
+  }, {
+    "name": "节流",
+    "alt": "throttle",
+    "link": "./src/view/debounce/throttle.html"
   }
 ]
